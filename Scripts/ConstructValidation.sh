20009f998b670383543f4c035c0130823019e632
ZenanXing <zxing001@ucr.edu>
2025-02-12 18:07:29 -0800

    Upload the latest version of the scripts.

---
+++ Scripts/ConstructValidation.sh
@@ -0,0 +1,187 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=20G
+#SBATCH --time=0-6:00:00
+#SBATCH -p intel # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
+
+### Define options
+# Define the default values
+SCRIPT_DIR=""
+INPUT_DIR=""
+OUTPUT_DIR=""
+OPTIMIZE_eE=false
+e_Pro=8
+E_Pro=11
+AA_Validation=false
+q_Chopper=13
+Medaka_Mod="r1041_e82_400bps_hac_v4.3.0"
+
+# Function to display help
+usage() {
+  echo "Usage:"
+  echo "./ConstructValidation.sh --script <DIR> -i <DIR> -o <DIR> [options]"
+  echo "Options:"
+  echo "  --script                      <DIR>   Scripts directory"
+  echo "  -i, --input                   <DIR>   Input directory"
+  echo "  -o, --output                  <DIR>   Output directory"
+  echo "  -q_chopper                    <INT>   Sets a minimum average Phred quality score for reads filtered by Chopper (default:13)"
+  echo "  -e                            <INT>   Barcode edit distance value (default:8, only if optimizing_eE is false)"
+  echo "  -E                            <INT>   Primer edit distance value (default:11, only if optimizing_eE is false)"
+  echo "  --medaka_model                <STR>   Base-calling model used for Medaka polishing (default:r1041_e82_400bps_hac_v4.3.0)"
+  echo "  --optimizing_eE                       Use the built-in function to optimize demultiplexing parameters (e & E) for maximum mapped reads"
+  echo "  --amino_acid_seq_validation           Translate the first ORF to its amino acid sequence to detect missense/silent mutations"
+  echo "  -h, --help                            Display this help"
+  exit 1
+}
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --script) SCRIPT_DIR="$2"; shift ;;
+        -i|--input) INPUT_DIR="$2"; shift ;;
+        -o|--output) OUTPUT_DIR="$2"; shift ;;
+        -q_chopper) q_Chopper="$2"; shift ;;
+        --medaka_model) Medaka_Mod="$2"; shift ;;
+        -e) 
+            if [ "$OPTIMIZE_eE" = true ]; then
+                echo "Error: Cannot define parameter e when optimizing_eE is true."
+                exit 1
+            fi
+            e_Pro="$2"
+            shift ;;
+        -E)
+            if [ "$OPTIMIZE_eE" = true ]; then
+                echo "Error: Cannot define parameter E when optimizing_eE is true."
+                exit 1
+            fi
+            E_Pro="$2"
+            shift ;;
+        --optimizing_eE) OPTIMIZE_eE=true ;;
+        --amino_acid_seq_validation) AA_Validation=true ;;
+        -h|--help) usage ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
+
+# Check if mandatory parameters are set
+if [[ -z "$INPUT_DIR" || -z "$OUTPUT_DIR" ]]; then
+    echo "Error: Input and output directories must be specified."
+    usage
+fi
+
+### Load the required modules
+module load parallel # Run the code in parallel
+module load minimap2 samtools bedtools # Alignment & Assembly
+module load seqtk # Polishing
+module load bcftools # Variant Calling
+module load emboss # Variant Identification
+module load muscle/3.8.31 # Pairwise Alignment
+
+### Making directory structure
+mkdir -p $OUTPUT_DIR/Analysis_Results/{Demultiplexing,VariantCalling/{beforePolishing,afterPolishing},Alignment,Assembly,PairwiseAlignment,InputFiles/references,OutputFiles/ntAlignment}
+mkdir -p $OUTPUT_DIR/Analysis_Results/Assembly/{bedtools/{sam,bed},racon/{1rd,2rd,3rd,sam},medaka/1rd}
+mkdir -p $OUTPUT_DIR/Analysis_Results/VariantCalling/afterPolishing/{racon/{1rd,2rd,3rd},medaka_1rd}
+mkdir -p $OUTPUT_DIR/Analysis_Results/PairwiseAlignment/combined_fasta
+
+### Set the working directory
+cd $OUTPUT_DIR/Analysis_Results
+
+### Generate InputFiles
+chmod 775 $SCRIPT_DIR/InputFiles.R
+Rscript $SCRIPT_DIR/InputFiles.R "$INPUT_DIR" "$OUTPUT_DIR" "$AA_Validation"
+
+if $AA_Validation; then
+    mkdir -p $OUTPUT_DIR/Analysis_Results/{VariantIdentification/{ReferenceORFs,AssembledORFs/{allORFs,selectedORF,combinedORFs}},OutputFiles/aaAlignment}
+    ### Variant Identification - pairwise alignment - aa - Get ORF for reference
+    parallel --jobs 100 --colsep '\t' "getorf -sequence ${OUTPUT_DIR}/Analysis_Results/InputFiles/references/{1}.fasta -outseq ${OUTPUT_DIR}/Analysis_Results/VariantIdentification/ReferenceORFs/{1}_orf.fasta -minsize {2} -find 1 -reverse N" :::: <(tail -n +2 "$OUTPUT_DIR/Analysis_Results/InputFiles/references_info.tsv")
+fi
+
+### Demultiplexing
+
+cd $OUTPUT_DIR/Analysis_Results/Demultiplexing
+chmod 775 $SCRIPT_DIR/minibar.py
+
+
+if $OPTIMIZE_eE; then
+    ## Get the edit distance between the most similar barcodes and primers (smallest edit distance), and store the numbers in e and E.
+    # edit distance of closest primers
+    edP=$($SCRIPT_DIR/minibar.py ../InputFiles/IndexCombination.txt -info primer | grep -o 'edit distance [0-9]*' | awk '{print $3}')
+    # length of the primers
+    maxP=$($SCRIPT_DIR/minibar.py $OUTPUT_DIR/Analysis_Results/InputFiles/IndexCombination.txt -info primer | grep -oP 'with lengths of \K[0-9]+')
+    # edit distance of closest barcodes
+    edB=$($SCRIPT_DIR/minibar.py ../InputFiles/IndexCombination.txt -info both | grep -o 'edit distance of [0-9]*' | awk '{print $4}')
+    # Create the config file contains all the combinations
+    echo -e "ArrayTaskID\te\tE" > config.txt
+    ArrayTaskID=0
+    for e in $(seq 1 $edB); do
+      for E in $(seq 1 $maxP); do
+        ArrayTaskID=$((ArrayTaskID+1))
+        # Append each combination along with ArrayTaskID to the file
+        echo -e "$ArrayTaskID\t$e\t$E" >> config.txt
+      done
+    done
+    # Create a file contains all the results
+    echo -e "e\tE\tMulti\tUnknown\tSorted\tTotalDmtplx\tSorted_pct\tTotalMapped\tMapped_pct\tTotalSamples\tTotalCompleteAssembledSamples\tTotalSets\tTotalCompleteAssembledSets\tSets_MeanDepth_5\tSamples_MeanDepth_5\tSets_MeanDepth_10\tSamples_MeanDepth_10\tSets_MeanDepth_15\tSamples_MeanDepth_15\tSets_MeanDepth_20\tSamples_MeanDepth_20" > e_n_E_Combination.tsv
+    # Run the job array script
+    conda init
+    source ~/bigdata/.conda/envs/chopper_env/bin/chopper
+    conda activate chopper_env
+    chmod 775 $SCRIPT_DIR/Demultiplexing_e_E.sh
+    job_id=$(sbatch --wait --array=1-$ArrayTaskID $SCRIPT_DIR/Demultiplexing_e_E.sh "$SCRIPT_DIR" "$INPUT_DIR" "$OUTPUT_DIR" "$q_Chopper")
+    job_id_f=$(echo $job_id | awk '{print $4}')
+    ## Export the log file
+    sacct --jobs=$job_id_f --format=JobID,JobName,Start,End,Elapsed | awk '{$1=$1; print}' OFS='\t' > log_dmtplx.tsv
+    
+    ## Remove all the log files
+    rm -rf *.out
+    
+    ## Extract the best combination of e & E
+    # Step 1: Sort the TSV file by  TotalMapped and then TotalSets(both descending) and get the top row
+    top_row=$(sort -t$'\t' -k19,19nr -k8,8nr e_n_E_Combination.tsv | head -n 1)
+    # Step 2: Extract the e and E columns from the top row
+    e_slt=$(echo "$top_row" | cut -f1)
+    E_slt=$(echo "$top_row" | cut -f2)
+    
+
+else
+    e_slt="$e_Pro"
+    E_slt="$E_Pro"
+
+fi
+
+
+## Sort the reads using minibar
+mkdir final
+cd final
+$SCRIPT_DIR/minibar.py ../../InputFiles/IndexCombination.txt $INPUT_DIR/passed_all.fastq -e $e_slt -E $E_slt -T -F -P ""
+
+## Filtering with chopper
+# Create the chopper_env using conda
+#conda create -n chopper_env -c conda-forge -c bioconda chopper
+conda activate chopper_env
+parallel 'chopper -q '$q_Chopper' -i {} > {.}_filtered.fastq' ::: *.fastq
+conda deactivate
+
+### Run analysis for individual samples
+cd $OUTPUT_DIR/Analysis_Results
+maxArrayid=$(awk 'NR > 1 { if ($1 > max) max = $1 } END { print max }' "$OUTPUT_DIR/Analysis_Results/InputFiles/config_sample.txt")
+chmod 775 $SCRIPT_DIR/Construct_Validation_per_Sample.sh
+module load medaka
+job_id=$(sbatch --wait --array=1-$maxArrayid $SCRIPT_DIR/Construct_Validation_per_Sample.sh "$SCRIPT_DIR" "$INPUT_DIR" "$OUTPUT_DIR" "$AA_Validation" "$Medaka_Mod")
+job_id_f=$(echo $job_id | awk '{print $4}')
+## Export the log file
+sacct --jobs=$job_id_f --format=JobID,JobName,Start,End,Elapsed | awk '{$1=$1; print}' OFS='\t' > log_analysis.tsv
+## Remove all the log files
+rm -rf *.out
+
+## Run the Summary.sh
+cd $OUTPUT_DIR/Analysis_Results
+chmod 775 $SCRIPT_DIR/Summary.sh
+job_id=$(sbatch --wait $SCRIPT_DIR/Summary.sh "$SCRIPT_DIR" "$INPUT_DIR" "$OUTPUT_DIR" "$AA_Validation")
+
+## Remove all the log files
+rm -rf *.out
---
+++ Scripts/Scripts/Construct_Validation_per_Sample.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2     # This will be the number of CPUs per individual array job
+#SBATCH --mem=20G     # This will be the memory per individual array job
+#SBATCH --time=0-04:00:00     # 1 hrs
+#SBATCH --job-name="analysis"
+
+# Set variables
+SCRIPT_DIR=$1
+INPUT_DIR=$2
+OUTPUT_DIR=$3
+AA_Validation=$4
+Medaka_Mod=$5
+
+# Specify the path to the config file
+config=$OUTPUT_DIR/Analysis_Results/InputFiles/config_sample.txt
+
+# SampleID
+SampleID=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+# ReferenceName
+Reference=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+
+### Load the required modules
+module load parallel # Run the code in parallel
+module load minimap2 samtools bedtools # Alignment & Assembly
+module load seqtk # Polishing
+module load bcftools # Variant Calling
+module load emboss # Variant Identification
+module load muscle/3.8.31 # Pairwise Alignment
+
+### Alignment
+## Change the directory and create required directory:
+cd $OUTPUT_DIR/Analysis_Results/Alignment
+## Alignment suing minimap2
+minimap2 -ax map-ont --secondary=no ../InputFiles/references/${Reference}.fasta ../Demultiplexing/final/${SampleID}_filtered.fastq > ${SampleID}.sam
+# Convert SAM to BAM
+samtools view -q 60 -F 2048 -S -b ${SampleID}.sam -o ${SampleID}.bam
+# Sort BAM files
+samtools sort ${SampleID}.bam -o ${SampleID}.sorted.bam
+# Index Sorted BAM files
+samtools index ${SampleID}.sorted.bam
+
+### Variant calling - before Polishing - using bcftools
+cd $OUTPUT_DIR/Analysis_Results/VariantCalling
+## Variant calling using bcftools and samtools
+bcftools mpileup -Ou -f ../InputFiles/references/${Reference}.fasta ../Alignment/${SampleID}.sorted.bam | bcftools call -cv -Ov > ./beforePolishing/${SampleID}_bP.vcf
+
+### Assembly
+cd $OUTPUT_DIR/Analysis_Results/Assembly
+## Draft Assembly
+# assembly using samtools
+samtools consensus -a -A -f fasta --show-ins no --show-del yes -o ./bedtools/sam/${SampleID}_sam.fasta ../Alignment/${SampleID}.sorted.bam
+# apply the variant using bedtools
+bedtools maskfasta -fi ./bedtools/sam/${SampleID}_sam.fasta -fo ./bedtools/bed/${SampleID}_draft.fasta -bed ../VariantCalling/beforePolishing/${SampleID}_bP.vcf
+
+### Polishing by Racon
+module load racon
+## racon polishing - 1rd
+minimap2 -ax map-ont ./bedtools/bed/${SampleID}_draft.fasta ../Demultiplexing/final/${SampleID}_filtered.fastq > ./racon/sam/${SampleID}_racon_1rd.sam
+racon -m 8 -x -6 -g -8 -w 500 ../Demultiplexing/final/${SampleID}_filtered.fastq ./racon/sam/${SampleID}_racon_1rd.sam ./bedtools/bed/${SampleID}_draft.fasta > ./racon/1rd/${SampleID}_racon_1rd.fasta
+
+## racon polishing - 2rd
+minimap2 -ax map-ont ./racon/1rd/${SampleID}_racon_1rd.fasta ../Demultiplexing/final/${SampleID}_filtered.fastq > ./racon/sam/${SampleID}_racon_2rd.sam
+racon -m 8 -x -6 -g -8 -w 500 ../Demultiplexing/final/${SampleID}_filtered.fastq ./racon/sam/${SampleID}_racon_2rd.sam ./racon/1rd/${SampleID}_racon_1rd.fasta > ./racon/2rd/${SampleID}_racon_2rd.fasta
+
+## racon polishing - 3rd
+minimap2 -ax map-ont ./racon/2rd/${SampleID}_racon_2rd.fasta ../Demultiplexing/final/${SampleID}_filtered.fastq > ./racon/sam/${SampleID}_racon_3rd.sam
+racon -m 8 -x -6 -g -8 -w 500 ../Demultiplexing/final/${SampleID}_filtered.fastq ./racon/sam/${SampleID}_racon_3rd.sam ./racon/2rd/${SampleID}_racon_2rd.fasta > ./racon/3rd/${SampleID}_racon_3rd.fasta
+
+### Polishing with medaka
+conda init
+source ~/bigdata/.conda/envs/chopper_env/bin/chopper
+
+module load medaka
+## 1rd
+medaka_consensus -f -x -i ../Demultiplexing/final/${SampleID}_filtered.fastq -d ./racon/3rd/${SampleID}_racon_3rd.fasta -o ./medaka/1rd/${SampleID} -m ${Medaka_Mod}; mv ./medaka/1rd/${SampleID}/consensus.fasta ./medaka/1rd/${SampleID}_medaka_1rd.fasta
+
+## Further processing the resulting fasta files so we can process the fasta files in the multiple alignments
+## Change the header of polished fasta files and saved it in a new fasta files
+# sam:
+awk -v new_header="${SampleID}_sam" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./bedtools/sam/${SampleID}_sam.fasta > ./bedtools/sam/${SampleID}_sam_f.fasta
+# draft:
+awk -v new_header="${SampleID}_draft" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./bedtools/bed/${SampleID}_draft.fasta > ./bedtools/bed/${SampleID}_draft_f.fasta
+# 1rd of racon polishing:
+awk -v new_header="${SampleID}_racon_1rd" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./racon/1rd/${SampleID}_racon_1rd.fasta > ./racon/1rd/${SampleID}_racon_1rd_f.fasta
+# 2rd of racon polishing:
+awk -v new_header="${SampleID}_racon_2rd" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./racon/2rd/${SampleID}_racon_2rd.fasta > ./racon/2rd/${SampleID}_racon_2rd_f.fasta
+# 3rd of racon polishing:
+awk -v new_header="${SampleID}_racon_3rd" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./racon/3rd/${SampleID}_racon_3rd.fasta > ./racon/3rd/${SampleID}_racon_3rd_f.fasta
+# 1rd of medaka polishing:
+awk -v new_header="${SampleID}_medaka_1rd" 'BEGIN {OFS = FS} NR == 1 {sub(/^>[^ ]*/, ">" new_header)} {print}' ./medaka/1rd/${SampleID}_medaka_1rd.fasta > ./medaka/1rd/${SampleID}_medaka_1rd_f.fasta
+
+
+### Variant Calling 
+cd $OUTPUT_DIR/Analysis_Results/VariantCalling
+module load medaka
+## after racon polishing
+# 1st round
+medaka tools consensus2vcf --mode NW --out_prefix ./afterPolishing/racon/1rd/${SampleID}_racon_1rd ../Assembly/racon/1rd/${SampleID}_racon_1rd.fasta ../InputFiles/references/${Reference}.fasta
+medaka tools classify_variants ./afterPolishing/racon/1rd/${SampleID}_racon_1rd.vcf
+# 2nd round
+medaka tools consensus2vcf --mode NW --out_prefix ./afterPolishing/racon/2rd/${SampleID}_racon_2rd ../Assembly/racon/2rd/${SampleID}_racon_2rd.fasta ../InputFiles/references/${Reference}.fasta
+medaka tools classify_variants ./afterPolishing/racon/2rd/${SampleID}_racon_2rd.vcf
+# 3rd round
+medaka tools consensus2vcf --mode NW --out_prefix ./afterPolishing/racon/3rd/${SampleID}_racon_3rd ../Assembly/racon/3rd/${SampleID}_racon_3rd.fasta ../InputFiles/references/${Reference}.fasta
+medaka tools classify_variants ./afterPolishing/racon/3rd/${SampleID}_racon_3rd.vcf
+## after medaka polishing
+# 1st round
+medaka tools consensus2vcf --mode NW --out_prefix ./afterPolishing/medaka_1rd/${SampleID}_medaka_1rd ../Assembly/medaka/1rd/${SampleID}_medaka_1rd.fasta ../InputFiles/references/${Reference}.fasta
+medaka tools classify_variants ./afterPolishing/medaka_1rd/${SampleID}_medaka_1rd.vcf
+
+
+### Visualization of the pairwise alignment - nt
+cd $OUTPUT_DIR/Analysis_Results/PairwiseAlignment
+## Align all the seqs by muscle
+# Combine all the fasta files in one file
+cat ../InputFiles/references/${Reference}.fasta ../Assembly/bedtools/sam/${SampleID}_sam_f.fasta ../Assembly/bedtools/bed/${SampleID}_draft_f.fasta ../Assembly/racon/1rd/${SampleID}_racon_1rd_f.fasta ../Assembly/racon/2rd/${SampleID}_racon_2rd_f.fasta ../Assembly/racon/3rd/${SampleID}_racon_3rd_f.fasta ../Assembly/medaka/1rd/${SampleID}_medaka_1rd_f.fasta > ./combined_fasta/${SampleID}_comb.fasta
+# Alignments of all the assemblies using Muscle3
+muscle -in ./combined_fasta/${SampleID}_comb.fasta -out ../OutputFiles/ntAlignment/${SampleID}_nt_align_muscle.html -html
+## Align sam&medaka_1rd to Reference by muscle
+# Combine all the fasta files in one file
+cat ../InputFiles/references/${Reference}.fasta ../Assembly/bedtools/sam/${SampleID}_sam_f.fasta ../Assembly/medaka/1rd/${SampleID}_medaka_1rd_f.fasta > ./combined_fasta/${SampleID}_comb.fasta
+# Alignments of all the assemblies using Muscle3
+muscle -in ./combined_fasta/${SampleID}_comb.fasta -out ../OutputFiles/ntAlignment/${SampleID}_nt_align_muscle_f.html -html
+
+## align seqs with references individually by emboss
+# Combine all the fasta files in one file
+cat ../Assembly/bedtools/sam/${SampleID}_sam_f.fasta ../Assembly/bedtools/bed/${SampleID}_draft_f.fasta ../Assembly/racon/1rd/${SampleID}_racon_1rd_f.fasta ../Assembly/racon/2rd/${SampleID}_racon_2rd_f.fasta ../Assembly/racon/3rd/${SampleID}_racon_3rd_f.fasta ../Assembly/medaka/1rd/${SampleID}_medaka_1rd_f.fasta > ./combined_fasta/${SampleID}_comb.fasta
+sed "s/\*/N/g" ./combined_fasta/${SampleID}_comb.fasta > ./combined_fasta/${SampleID}_comb_cleaned.fasta
+# Alignments of all the assemblies using emboss
+needle -asequence ../InputFiles/references/${Reference}.fasta -bsequence ./combined_fasta/${SampleID}_comb_cleaned.fasta -gapopen 10 -gapextend 0.5 -outfile ../OutputFiles/ntAlignment/${SampleID}_nt_align_emboss.txt
+
+
+if $AA_Validation; then
+    ### Variant Identification - pairwise alignment - aa
+    cd $OUTPUT_DIR/Analysis_Results/VariantIdentification
+    
+    ## Get all ORFs for the polished consensus sequences
+    # Create folders for each sample
+    mkdir -p $OUTPUT_DIR/Analysis_Results/VariantIdentification/AssembledORFs/{allORFs/${SampleID},selectedORF/${SampleID}}
+    
+    # sam
+    sed "s/N//g; s/\*//g" ../Assembly/bedtools/sam/${SampleID}_sam_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_sam_all_orf.fasta -minsize 3 -find 1 -reverse N 
+    # draft
+    sed "s/N//g; s/\*//g" ../Assembly/bedtools/bed/${SampleID}_draft_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_draft_all_orf.fasta -minsize 3 -find 1 -reverse N
+    # racon - 1rd
+    sed "s/N//g; s/\*//g" ../Assembly/racon/1rd/${SampleID}_racon_1rd_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_racon_1rd_all_orf.fasta -minsize 3 -find 1 -reverse N
+    # racon - 2rd
+    sed "s/N//g; s/\*//g" ../Assembly/racon/2rd/${SampleID}_racon_2rd_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_racon_2rd_all_orf.fasta -minsize 3 -find 1 -reverse N
+    # racon - 3rd
+    sed "s/N//g; s/\*//g" ../Assembly/racon/3rd/${SampleID}_racon_3rd_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_racon_3rd_all_orf.fasta -minsize 3 -find 1 -reverse N
+    # medaka - 1rd
+    sed "s/N//g; s/\*//g" ../Assembly/medaka/1rd/${SampleID}_medaka_1rd_f.fasta | getorf -sequence stdin -outseq ./AssembledORFs/allORFs/${SampleID}/${SampleID}_medaka_1rd_all_orf.fasta -minsize 3 -find 1 -reverse N
+
+    ## Find the ORFs with the smallest start site:
+    find ./AssembledORFs/allORFs/${SampleID}/ -name '*.fasta' | parallel --jobs 4 'awk '\''/^>/ {header = $0; match($0, /\[([0-9]+)/, m); start = m[1]; if (min_start == "" || start < min_start) {min_start = start; min_header = header; min_seq = "";}} /^[^>]/ {if (start == min_start) {min_seq = min_seq $0;}} END {output_file = "./AssembledORFs/selectedORF/" substr(FILENAME, length("./AssembledORFs/allORFs/") + 1); sub("_all_orf.fasta", "_orf.fasta", output_file); min_header = gensub(/_[0-9]+( \[.*\])/, "\\1", "1", min_header); print min_header "\n" min_seq > output_file;}'\'' {}'
+    
+    ## Protein sequences alignment to see if it is a missense mutation
+    
+    ## align all the seqs by muscle
+    # Combine all the fasta files in one file
+    cat ReferenceORFs/${Reference}_orf.fasta AssembledORFs/selectedORF/${SampleID}/*.fasta > AssembledORFs/combinedORFs/${SampleID}_comb.fasta
+    # Alignments of all the assemblies using Muscle3
+    muscle -in ./AssembledORFs/combinedORFs/${SampleID}_comb.fasta -out ../OutputFiles/aaAlignment/${SampleID}_aa_align_muscle.html -html
+    ## align sam&medaka_1rd to Reference by muscle
+    # Combine all the fasta files in one file
+    cat ReferenceORFs/${Reference}_orf.fasta AssembledORFs/selectedORF/${SampleID}/${SampleID}_sam_orf.fasta  AssembledORFs/selectedORF/${SampleID}/${SampleID}_medaka_1rd_orf.fasta > AssembledORFs/combinedORFs/${SampleID}_comb.fasta
+    # Alignments of all the assemblies using Muscle3
+    muscle -in ./AssembledORFs/combinedORFs/${SampleID}_comb.fasta -out ../OutputFiles/aaAlignment/${SampleID}_aa_align_muscle_f.html -html
+    
+    ## align seqs with references individually by emboss
+    # Combine all the fasta files in one file
+    cat AssembledORFs/selectedORF/${SampleID}/*.fasta > AssembledORFs/combinedORFs/${SampleID}_comb.fasta
+    # Alignments of all the assemblies using emboss
+    needle -asequence ReferenceORFs/${Reference}_orf.fasta -bsequence ./AssembledORFs/combinedORFs/${SampleID}_comb.fasta -gapopen 10 -gapextend 0.5 -outfile ../OutputFiles/aaAlignment/${SampleID}_aa_align_emboss.txt
+fi
